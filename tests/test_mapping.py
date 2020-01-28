fd7cee1526cf253e9397faff15ce9788bdd36c4b
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-01-28 11:34:03 +0100

    Reset repo

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -12,7 +12,7 @@ def test_mapping():
         os.makedirs("complete_mapping", exist_ok=True)
         # Testing without cafa filtering
         mp = mapping(month, cafa4_only=False)
-        path = f"complete_mapping/{month}.tsv"
+        path = f"complete_mapping/{month}.tsv.gz"
         if not os.path.exists(path):
             mp.to_csv(path, index=False, sep="\t")
         pd.testing.assert_frame_equal(mp, pd.read_csv(path, sep="\t"))
@@ -27,7 +27,7 @@ def test_cafa_only_mapping():
 
         # Testing with cafa filtering
         mp_cafa = mapping(month, cafa4_only=True)
-        path = f"complete_mapping/{month}_cafa_only.tsv"
+        path = f"complete_mapping/{month}_cafa_only.tsv.gz"
         if not os.path.exists(path):
             mp_cafa.to_csv(path, index=False, sep="\t")
         pd.testing.assert_frame_equal(mp_cafa, pd.read_csv(path, sep="\t"))
