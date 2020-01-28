4597faad9b72ab84451db3458672e970d653d967
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-01-28 11:24:57 +0100

    Now using multiprocessing rendeing

--- hpo_downloader/hpo/hpo_mapping.py
+++ hpo_downloader/hpo/hpo_mapping.py
@@ -4,18 +4,18 @@ import pandas as pd
 def hpo_mapping() -> pd.DataFrame:
     """Return dataframe containing phenotype HPO mapping."""
     df = pd.read_csv(
-        "http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt",
+        "http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt",
         sep="\t"
     )
     columns = df.columns[0].split(": ")[-1].split("<tab>")
     df = df.reset_index()
     df.columns = columns
     df = df.drop(columns=[
-        "entrez-gene-symbol",
-        "HPO-Term-Name"
+        "Gene-Name",
+        "HPO-Name"
     ])
     df = df.rename(columns={
-        "entrez-gene-id": "gene_id",
-        "HPO-Term-ID": "hpo_id"
+        "Gene-ID": "gene_id",
+        "HPO-ID": "hpo_id"
     })
     return df
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,33 +1,37 @@
+from typing import Dict
 from hpo_downloader import mapping
 from tqdm.auto import tqdm
 import pandas as pd
 import os
+from itertools import product
+from multiprocessing import Pool, cpu_count
 
 
-def test_mapping():
-    for month in tqdm(
-        ("october", "november", "december"),
-        desc="Parsing monthly releases"
-    ):
-        os.makedirs("complete_mapping", exist_ok=True)
-        # Testing without cafa filtering
-        mp = mapping(month, cafa4_only=False)
-        path = f"complete_mapping/{month}.tsv"
-        if not os.path.exists(path):
-            mp.to_csv(path, index=False, sep="\t")
-        pd.testing.assert_frame_equal(mp, pd.read_csv(path, sep="\t"))
-
-
-def test_cafa_only_mapping():
-    for month in tqdm(
-        ("october", "november", "december"),
-        desc="Parsing monthly releases"
-    ):
-        os.makedirs("complete_mapping", exist_ok=True)
-
-        # Testing with cafa filtering
-        mp_cafa = mapping(month, cafa4_only=True)
+def job(task: Dict):
+    month = task["month"]
+    cafa4_only = task["cafa4_only"]
+    mp_cafa = mapping(month, cafa4_only=cafa4_only)
+    if cafa4_only:
         path = f"complete_mapping/{month}_cafa_only.tsv"
-        if not os.path.exists(path):
-            mp_cafa.to_csv(path, index=False, sep="\t")
-        pd.testing.assert_frame_equal(mp_cafa, pd.read_csv(path, sep="\t"))
+    else:
+        path = f"complete_mapping/{month}.tsv"
+    if not os.path.exists(path):
+        mp_cafa.to_csv(path, index=False, sep="\t")
+    pd.testing.assert_frame_equal(mp_cafa, pd.read_csv(path, sep="\t"))
+
+
+def test_all_mapping():
+    months = ["october", "november", "december"]
+    tasks = [
+        {
+            "month": month,
+            "cafa4_only": cafa4_only
+        }
+        for month, cafa4_only in product(months, [True, False])
+    ]
+    os.makedirs("complete_mapping", exist_ok=True)
+    with Pool(cpu_count()) as p:
+        list(tqdm(p.imap(
+            job,
+            tasks
+        ), total=len(tasks), desc="Rendering mapping"))
